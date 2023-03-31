59b3d7d2094ff3df280792ef8da768a2c28ebe35
Alan Altamira <alan>
2023-03-31 13:28:43 -0600

    Initial commit -app

---
+++ app/src/main/java/royal/master/academy/grupo_a/MainActivity.kt
@@ -0,0 +1,39 @@
+package royal.master.academy.grupo_a
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import royal.master.academy.grupo_a.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    /* */
+    private lateinit var binding : ActivityMainBinding
+
+
+    /** Un Activy es una vista de tu Aplicacion*/
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        /*Indicar que vista se mostrara*/
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        /*Empezar nuestro codigo*/
+        setName("Hola a todos!")
+
+    }
+
+    /** */
+    private fun setName(name : String){
+
+        /*Decimos a la vista que queremos poner en el atributo [texto]
+        * de nuestro Objeto [TextView](de la vista) por medio del objeto [binding]
+        * el cual contiene todas las referencias de nuestra vista (xml)
+        * */
+        binding.txtTitle.text = name
+
+    }
+
+
+}
